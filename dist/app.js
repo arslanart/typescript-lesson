57b0cf3c8949c684276ddba45f05fe1abeeb582b
araslanart <arslanart913@gmail.com>
2025-09-02 16:26:27 +0700

    📝 ปรับปรุงข้อความใน console.log และเพิ่มฟังก์ชัน total ในไฟล์ app.ts (คำสั่ง watch mode "tsc -w")

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,6 @@
 "use strict";
-console.log("Hello, Application!");
+console.log("Hello, This is app.ts");
+function total(a, b) {
+    return a + b;
+}
+console.log(total(10, 20));
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,7 @@
-console.log("Hello, Application!");
+console.log("Hello, This is app.ts");
+
+function total(a:number, b:number) {
+    return a + b;
+}
+
+console.log(total(10, 20));
