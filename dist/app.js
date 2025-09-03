b083eb4756d0e0dad3ffea4db0c84b9e07d9d0a5
araslanart <arslanart913@gmail.com>
2025-09-03 18:04:54 +0700

    📝 TypeAssertion

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,4 @@
 "use strict";
-// Any และ Unknown
-function formartNumber(num) {
-    return num.toFixed(2);
-}
-let money = 32168.165; // ตัวแปรชนิด unknown
-// ต้องตรวจสอบชนิดข้อมูลก่อนใช้งาน
-if (typeof money === "number") {
-    console.log(formartNumber(money));
-}
+let username;
+username = "John";
+console.log(username.toUpperCase()); // type assertion
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,3 @@
-// Any และ Unknown
-function formartNumber(num: number) {
-  return num.toFixed(2);
-}
-
-let money: unknown = 32168.165; // ตัวแปรชนิด unknown
-
-// ต้องตรวจสอบชนิดข้อมูลก่อนใช้งาน
-if (typeof money === "number") {
-  console.log(formartNumber(money));
-}
-
-
+let username: unknown;
+username = "John";
+console.log((username as string).toUpperCase()); // type assertion
