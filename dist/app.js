0bbf540154286d28dbb211856d0d261437689d6b
araslanart <arslanart913@gmail.com>
2025-09-05 17:17:15 +0700

    ฟังก์ชันที่คืนค่าจาก object

--- dist/app.js
+++ dist/app.js
@@ -1,21 +1,13 @@
 "use strict";
-//OOP
-//ไม่ระบุชนิดข้อมูล
-const person = {
-    name: "John",
-    age: 30,
-};
-//ระบุชนิดข้อมูล
-const person2 = {
-    name: "John",
-    age: 30,
-};
+// ฟังก์ชันที่คืนค่าจาก object
 const position = {
     lat: 10,
-    long: 20
+    long: 20,
 };
-//ฟังก์ชันรับค่าจาก object
-function showDetails(data) {
-    console.log(`My name is ${data.name} and I'm ${data.age} years old.`);
+function randomPosition() {
+    return {
+        lat: Math.random(),
+        long: Math.random()
+    };
 }
-showDetails(person2);
+console.log(randomPosition());
--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,13 @@
-//OOP
-
-//ไม่ระบุชนิดข้อมูล
-const person = {
-  name: "John",
-  age: 30,
-};
-
-//ระบุชนิดข้อมูล
-const person2: { name: string; age: number } = {
-  name: "John",
-  age: 30,
-};
-
-const position: {lat:number, long:number} = {
+// ฟังก์ชันที่คืนค่าจาก object
+const position: { lat: number; long: number } = {
   lat: 10,
-  long: 20
-}
+  long: 20,
+};
 
-//ฟังก์ชันรับค่าจาก object
-function showDetails(data: {name:string, age:number}){
-  console.log(`My name is ${data.name} and I'm ${data.age} years old.`);
+function randomPosition(): { lat: number; long: number } {
+  return { 
+    lat: Math.random(), 
+    long: Math.random()
+  };
 }
-
-showDetails(person2);
+console.log(randomPosition());
