8e64e7bea36841c93a27995d3543e36921cfcde2
araslanart <arslanart913@gmail.com>
2025-09-05 17:12:01 +0700

    ฟังก์ชันรับค่าจาก object

--- dist/app.js
+++ dist/app.js
@@ -14,3 +14,8 @@ const position = {
     lat: 10,
     long: 20
 };
+//ฟังก์ชันรับค่าจาก object
+function showDetails(data) {
+    console.log(`My name is ${data.name} and I'm ${data.age} years old.`);
+}
+showDetails(person2);
--- src/app.ts
+++ src/app.ts
@@ -16,3 +16,10 @@ const position: {lat:number, long:number} = {
   lat: 10,
   long: 20
 }
+
+//ฟังก์ชันรับค่าจาก object
+function showDetails(data: {name:string, age:number}){
+  console.log(`My name is ${data.name} and I'm ${data.age} years old.`);
+}
+
+showDetails(person2);
